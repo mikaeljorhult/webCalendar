4761d791327ecea594a801db56a75872d634bbd5
Mikael Jorhult <mikael@jorhult.se>
2014-02-26 21:28:22 +0100

    Basic routes.

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,35 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+ * Home.
+ */
+Route::get( '/', array( 'as' => 'home', 'uses' => 'AppController@index' ) );
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+/*
+ * Log in and out.
+ */
+Route::get( 'login', [ 'as' => 'login', 'uses' => 'SessionsController@create' ] );
+Route::get( 'logout', [ 'as' => 'logout', 'uses' => 'SessionsController@destroy' ] );
+Route::resource( 'sessions', 'SessionsController', [ 'only' => [ 'create', 'destroy', 'store' ] ] );
+
+/*
+ * Update courses.
+ */
+Route::get( 'update', array( 'as' => 'update', 'uses' => 'AppController@update', 'before' => 'auth' ) );
+Route::get( 'update/{id}', array( 'as' => 'update.course', 'uses' => 'AppController@update', 'before' => 'auth' ) );
+
+/*
+ * Administration Panel.
+ */
+Route::group( array( 'prefix' => 'admin' ), function() {
+	Route::get( '/', array( 'as' => 'admin', 'uses' => 'AppController@admin' ) );
+	Route::resource( 'course', 'CourseController' );
+	Route::resource( 'module', 'ModuleController' );
+	Route::resource( 'lesson', 'LessonController' );
+	Route::resource( 'user', 'UserController' );
+} );
+
+/*
+ * Display course schedule.
+ */
+Route::get( 'schedule/{code}', array( 'uses' => 'CourseController@display' ) );
