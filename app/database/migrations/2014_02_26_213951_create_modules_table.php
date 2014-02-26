be059265f574a00af10184e38c9373a17a37bce9
Mikael Jorhult <mikael@jorhult.se>
2014-02-26 21:40:55 +0100

    Migration for creating modules table.

---
+++ app/database/migrations/2014_02_26_213951_create_modules_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModulesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create( 'modules', function( Blueprint $table ) {
+			$table->engine = 'InnoDB';
+			$table->increments( 'id' )->unsigned();
+			
+			$table->integer( 'course_id' )->unsigned();
+			
+			$table->string( 'name', 75 );
+			$table->string( 'short_name', 10 );
+			$table->string( 'calendar' );
+			
+			$table->timestamps();
+			
+			$table->index( 'name' );
+		} );
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists( 'modules' );
+	}
+
+}
