a86431264b6cd6900d42ffd558cf77585fb49fcc
Mikael Jorhult <mikael@jorhult.se>
2014-02-26 21:39:27 +0100

    Migration for creating courses table.

---
+++ app/database/migrations/2014_02_26_213743_create_courses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create( 'courses', function( Blueprint $table ) {
+			$table->engine = 'InnoDB';
+			$table->increments( 'id' )->unsigned();
+			
+			$table->string( 'code', 10 );
+			$table->string( 'name', 75 );
+			
+			$table->date( 'start_date' );
+			$table->date( 'end_date' );
+			
+			$table->timestamps();
+			
+			$table->index( 'code' );
+		} );
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists( 'courses' );
+	}
+
+}
