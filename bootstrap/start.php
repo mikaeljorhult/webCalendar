19084a6f29e33fab0fdf53d8723473173cf06e3e
Mikael Jorhult <mikael@jorhult.se>
2014-03-12 11:57:38 +0100

    Another possible host name for local machine.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -25,7 +25,7 @@ $app = new Illuminate\Foundation\Application;
 */
 
 $env = $app->detectEnvironment( array(
-	'local' => array( 'Jorhult.local' ),
+	'local' => array( 'Jorhult.local', 'jorhult.lan' ),
 ) );
 
 /*
