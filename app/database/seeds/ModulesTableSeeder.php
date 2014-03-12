ca5cde0c2169a85e1228a34d6873e2b554e3d156
Mikael Jorhult <mikael@jorhult.se>
2014-03-12 20:43:06 +0100

    Make sure that end date is larger than start date.

--- app/database/seeds/ModulesTableSeeder.php
+++ app/database/seeds/ModulesTableSeeder.php
@@ -7,10 +7,12 @@ class ModulesTableSeeder extends Seeder {
 		$faker = Faker::create();
 		
 		foreach ( range( 1, 30 ) as $index ) {
+			$start_date = $faker->dateTimeBetween( '-1 month', '+1 month' );
+			
 			Module::create( [
 				'name' => trim( $faker->sentence( rand( 1, 3 ) ), ' .' ),
-				'start_date' => $faker->dateTimeBetween( '-1 month', '+1 month' )->format( 'Y-m-d' ),
-				'end_date' => $faker->dateTimeBetween( '-1 month', '+1 month' )->format( 'Y-m-d' ),
+				'start_date' => $start_date->format( 'Y-m-d' ),
+				'end_date' => $faker->dateTimeBetween( $start_date, '+1 month' )->format( 'Y-m-d' ),
 				'calendar' => 'http://google.se'
 			] );
 		}
