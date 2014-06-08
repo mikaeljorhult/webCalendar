83fd2ff1d2beba1c83002ee0895dfc50cb6ece45
Mikael Jorhult <mikael@jorhult.se>
2014-06-08 18:33:03 +0200

    Consider Homestead a development machine.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -25,7 +25,7 @@ $app = new Illuminate\Foundation\Application;
 */
 
 $env = $app->detectEnvironment( array(
-	'local' => array( 'Jorhult.local', 'jorhult.lan', 'mac-mjr.local' ),
+	'local' => array( 'Jorhult.local', 'jorhult.lan', 'mac-mjr.local', 'homestead' ),
 ) );
 
 /*
