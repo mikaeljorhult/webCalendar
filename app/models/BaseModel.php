3607176d1cf4eb47d41d0f33838401169d45db6a
Mikael Jorhult <mikael@jorhult.se>
2015-01-04 21:35:33 +0100

    Allow validation to ignore current database row.
    Fixes #4.

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -4,7 +4,7 @@ class BaseModel extends Eloquent {
 	public $errors;
 	
 	public function validate() {
-		$v = Validator::make( $this->attributes, static::$rules );
+		$v = Validator::make( $this->attributes, $this->getRules() );
 		$v->setAttributeNames( static::$niceNames );
 		
 		if ( $v->passes() ) {
@@ -15,5 +15,17 @@ class BaseModel extends Eloquent {
 		$this->errors = $v->messages();
 		
 		return false;
+	}
+	
+	public function getRules() {
+		return $this->parseRules( array_map( [ $this, 'parseRules' ], static::$rules ) );
+	}
+	
+	public function parseRules( $value ) {
+		if ( is_array( $value ) ) {
+			return array_map( [ $this, 'parseRules' ], $value );
+		}
+		
+		return ( isset( $this->attributes[ 'id' ] ) ? str_replace( '{{id}}', $this->attributes[ 'id' ], $value ) : $value );
 	}
 }
