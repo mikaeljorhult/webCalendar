09ea715b7117766c400fb156520bfa8b30323c77
Mikael Jorhult <mikael@jorhult.se>
2015-12-11 22:01:02 +0100

    Translate weekday names in schedules.

---
+++ resources/lang/sv/weekdays.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Monday' => 'måndag',
+    'Tuesday' => 'tisdag',
+    'Wednesday' => 'onsdag',
+    'Thursday' => 'torsdag',
+    'Friday' => 'fredag',
+    'Saturday' => 'lördag',
+    'Sunday' => 'söndag'
+];
