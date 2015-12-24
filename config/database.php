6a2c2eae9173a48e9fb4aadf155e70bc183d8483
Mikael Jorhult <mikael@jorhult.se>
2015-12-24 22:08:58 +0100

    Run tests in memory.

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
 
         'sqlite' => [
             'driver'   => 'sqlite',
-            'database' => storage_path('testing.sqlite'),
+            'database' => ':memory:',
             'prefix'   => '',
         ],
 
