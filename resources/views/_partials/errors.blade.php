517712164cc63ff5b2cce3884211af9c7ebcb4d6
Mikael Jorhult <mikael@jorhult.se>
2015-12-25 20:10:28 +0100

    Only show errors if errors variable is present.

--- resources/views/_partials/errors.blade.php
+++ resources/views/_partials/errors.blade.php
@@ -1,6 +1,6 @@
-@if ( $errors->count() > 0 )
+@if (isset($errors) && $errors->count() > 0)
 	<ul class="validation-errors">
-		@foreach ( $errors->all( '<li>:message</li>' ) as $error )
+		@foreach ($errors->all('<li>:message</li>') as $error)
 			{!! $error !!}
 		@endforeach
 	</ul>
